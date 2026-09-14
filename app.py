3c4d4575b839b2487b27064a04b846e95bff5ade
Mahonee <bayros291@gmail.com>
2026-09-14 16:26:19 +0300

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ import urllib.request
 import requests
 import streamlit as st
 import streamlit.components.v1 as components
+st.set_page_config(page_title="Hisse Takip", page_icon="logo.png")
 
 VERI_DOSYASI = "hisseler.json"
 BILDIRIM_DOSYASI = "bildirim_durumu.json"
